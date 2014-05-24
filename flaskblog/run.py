9b9dd82d649f8ba8cb28f985fbf0289d51a25abb
Jacky <ecstorm@gmail.com>
2014-05-24 20:10:42 +0800

    update

--- flaskblog/run.py
+++ flaskblog/run.py
@@ -1,7 +1,9 @@
 from app import app
 from flaskext.markdown import Markdown
+from werkzeug.contrib.fixers import ProxyFix
 
 if __name__ == '__main__':
 	Markdown(app)
+	app.wsgi_app = ProxyFix(app.wsgi_app)
 	app.debug = True
 	app.run()
